921f30aafa53381eb485f78cd3108f7a9c849bb1
Samyar-Sharafi <138717806+Samyar-Sharafi@users.noreply.github.com>
2025-07-02 13:22:09 +0330

    Add files via upload

---
+++ Mini_Projects/Mini_OS/main/main.py
@@ -0,0 +1,27 @@
+
+from tqdm import tqdm
+import time
+import random as r
+import os
+
+def starter ():
+    print('''
+┏━┓┏━┓ ┏━━┓ ┏━┓︱┏┓ ┏━━┓           ┏━━━┓ ┏━━━┓
+┃┃┗┛┃┃ ┗┫┣┛ ┃┃┗┓┃┃ ┗┫┣┛           ┃┏━┓┃ ┃┏━┓┃
+┃┏┓┏┓┃ ︱┃┃︱ ┃┏┓┗┛┃ ︱┃┃︱        ┃︱┃┃ ┃┗━━┓
+┃┃┃┃┃┃ ︱┃┃︱ ┃┃┗┓┃┃ ︱┃┃︱        ┃┃︱┃┃ ┗━━┓┃
+┃┃┃┃┃┃ ┏┫┣┓ ┃┃︱┃┃┃ ┏┫┣┓           ┃┗━┛┃ ┃┗━┛┃
+┗┛┗┛┗┛ ┗━━┛ ┗┛︱┗━┛ ┗━━┛           ┗━━━┛ ┗━━━┛
+''')
+    
+    for i in tqdm(range(100), desc="BOOTING"):
+        time.sleep(r.uniform(0.000000000000000000001, 0.10000000000000000000))
+    try:
+            os.system("python C:/Users/DELL/Documents/Python/WorkSpace/projects/Mini_OS/main/console.py")
+    except Exception as e:
+            print("ERROR FOUND: ")
+            print(f"{e}")
+            exit()
+
+starter()
+
